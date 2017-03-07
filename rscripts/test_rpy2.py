6408b77f5144d30f3a73fa20097bcc9d36f5e8b1
ned <daniele.nerini@meteoswiss.ch>
2017-03-07 16:17:35 +0100

    added example to read RZC data from python

--- rscripts/test_rpy2.py
+++ rscripts/test_rpy2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import rpy2.robjects as robjects
+import sys
 
 robjects.r('''
        source('read_rzc.r')
@@ -8,10 +9,10 @@ robjects.r('''
 
 r_readRZC = robjects.globalenv['readRZC']
 
-filename = '/scratch/ned/data/2016/16107/RZC161072300VL.801'
+filename = '/scratch/ned/data/2016/16107/RZC161072100VL.801'
 
-precip = r_readRZC(filename)
-precip = np.array(precip)
+precip = r_readRZC(filename) # mm/h
+precip = np.array(precip) 
 
 
 plt.imshow(precip.T)
