c0f53e8d50af071eaab9d4ee26b159ffe144c639
ned <daniele.nerini@meteoswiss.ch>
2017-11-14 16:16:01 +0100

    it now includes some doc

--- pymodules/run_fieldextra.py
+++ pymodules/run_fieldextra.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+"""
+DESCRIPTION
+
+    Utils for extracting COSMO data using fieldexta. Currently only analysis for COSMO-2 and COSMO-7 are supported.
+
+AUTHOR
+
+    Daniele Nerini <daniele.nerini@meteoswiss.ch>
+
+VERSION
+
+    1.0
+"""
 
 import datetime
 import os
@@ -10,24 +23,27 @@ import fnmatch
 import numpy as np
 import netCDF4
 
-'''
-NOTE: COSMO-1 forecasts available starting 30.09.2015 12 UTC only. 
-
-To use fieldextra it is necessary to relax system limits imposed on size of
-stack, both for sequential mode and for OpenMP mode. Please exedcute these commands before you start fieldextra on kesch:
-
-# bash:
-ulimit -s unlimited              # unlimit stack size
-export OMP_STACKSIZE=500M        # increase OpenMP stack size
-
-'''
-
 def run_fieldextra_analysis(analysisTimeStr, 
                     gribDir,
                     outBaseDir='',
                     fieldName='HZEROCL',
                     modelName='cosmo-7'):
-
+    '''
+    Wrap to the fieldextra program to read the COSMO-2/7 analyses and store them as netcdf.
+    
+    Parameters
+    ----------
+    analysisTimeStr : str
+        Date of the analysis day (YYYYMMDD). Will extract all hourly analyses of that day.
+        Can use <datetimeobject>.strftime(%Y%m%d) to print it correctly.
+    gribDir : str
+        Full path to the folder containing the untared grib cosmo data.
+    fieldName : str ['HZEROCL', 'TOT_PREC']
+        Variable to extract. Currently only 'HZEROCL' and 'TOT_PREC' are supported.
+    modelName : str ['cosmo-7', 'cosmo-2']
+        COSMO model name.
+    '''
+                    
     # store current working directory
     cwd = os.getcwd()
     # move to script directory
@@ -128,13 +144,36 @@ def run_fieldextra_analysis(analysisTimeStr,
     return outFile
    
 def insert_variable(fileIn,fileOut,textToSearch,textToReplace):
+    '''
+    Function to fill the fieldextra config file template with relevant parameters.
+    
+    Parameters
+    ----------
+    fileIn : str
+        Filename of the config template file.
+    fileOut : str
+        New filenamne.
+    textToSearch : str 
+        Placeholder to replace.
+    textToReplace : str
+        String to insert.
+    '''
     with open(fileIn) as f_in:
         text = f_in.read()
     with open(fileOut, 'w') as f_out:    
         f_out.write(re.sub(textToSearch, lambda x: '{}'.format(textToReplace), text))
 
 def load_3darray_netcdf(filename):
-
+    '''
+    Read the netcdf file that is produced by fieldextra.
+    The dimension of the output numpy array will be [time,y,x].
+    Note that the reference is the lower left corner, so for most applications you will have to flip upside-down the images with np.flipud().
+    
+    Parameters
+    ----------
+    filename : str
+        Filename of the netcdf file.
+    '''
     # read netcdf file
     nc_fid = netCDF4.Dataset(filename, 'r', format='NETCDF4')
     variableNames = [str(var) for var in nc_fid.variables]
